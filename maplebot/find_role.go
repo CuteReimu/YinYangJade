15913cdfc7876653a8f0f562f9f6a7ef13e28ece
CuteReimu <415551921@qq.com>
2024-05-20 15:19:12 +0800

    查询角色显示经验变化

--- maplebot/find_role.go
+++ maplebot/find_role.go
@@ -45,7 +45,8 @@ func findRole(name string) MessageChain {
 		levelExp    = ""
 		legionLevel = "0"
 		imgUrl      = ""
-		chartData   gjson.Result
+		expData     gjson.Result
+		levelData   gjson.Result
 	)
 	nameMatch := nameRegex.FindSubmatchIndex(body)
 	if nameMatch != nil {
@@ -77,12 +78,15 @@ func findRole(name string) MessageChain {
 		body = body[dataMatch[1]:]
 		index := bytes.Index(body, []byte("},"))
 		rawData := "{" + string(body[:index]) + "}"
-		chartData = gjson.Parse(rawData)
+		chartData := gjson.Parse(rawData)
 		a := chartData.Get("datasets").Array()
-		if len(a) > 0 && a[0].Get("label").String() != "Exp" {
-			chartData = gjson.Result{}
-		} else {
-			break
+		if len(a) > 0 {
+			switch a[0].Get("label").String() {
+			case "Level":
+				levelData = chartData
+			case "Exp":
+				expData = chartData
+			}
 		}
 	}
 
@@ -93,31 +97,76 @@ func findRole(name string) MessageChain {
 
 	s := fmt.Sprintf("角色名：%s\n职业：%s\n等级：%s\n联盟：%s\n", rawName, class, levelExp, legionLevel)
 
-	values := make([]float64, 0, 14)
-	labels := make([]string, 0, 14)
-	a := chartData.Get("datasets").Array()
+	expValues := make([]float64, 0, 30)
+	levelValues := make([]float64, 0, 30)
+	labels := make([]string, 0, 30)
+	a := expData.Get("datasets").Array()
 	if len(a) > 0 {
 		datasets := a[0].Get("data").Array()
-		for i, label := range chartData.Get("labels").Array() {
+		for i, label := range expData.Get("labels").Array() {
 			if s := label.String(); len(s) > 0 {
 				labels = append(labels, s)
-				values = append(values, float64(datasets[i].Int()))
+				expValues = append(expValues, float64(datasets[i].Int()))
 			}
 		}
-		if slices.ContainsFunc(values, func(f float64) bool { return f != 0 }) {
+		maxValue := slices.Max(expValues)
+		digits := int(math.Floor(math.Log10(maxValue))) + 1
+		factor := math.Pow(10, float64(digits-1))
+		highest := math.Floor(maxValue / factor)
+		if highest < 2 {
+			factor /= 5
+		} else if highest < 5 {
+			factor /= 2
+		}
+		maxValue = math.Ceil(maxValue/factor) * factor
+		divideCount := int(maxValue / factor)
+		var yAxisOptions []YAxisOption
+		yAxisOptions = append(yAxisOptions, YAxisOption{Min: NewFloatPoint(0), Max: &maxValue, DivideCount: divideCount, Unit: 1})
+		aLevel := levelData.Get("datasets").Array()
+		if len(aLevel) > 0 {
+			datasetsLevel := aLevel[0].Get("data").Array()
+			for i, label := range levelData.Get("labels").Array() {
+				if s := label.String(); len(s) > 0 && i < len(datasetsLevel) {
+					levelValues = append(levelValues, datasetsLevel[i].Float())
+				}
+			}
+			for i := range levelValues {
+				if levelValues[i] == 0 && i > 0 {
+					levelValues[i] = levelValues[i-1]
+				}
+			}
+			for i := len(levelValues) - 2; i >= 0; i-- {
+				if levelValues[i] == 0 {
+					levelValues[i] = levelValues[i+1]
+				}
+			}
+			for _, diff := range []float64{0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100} {
+				minLevel, maxLevel := math.Floor(slices.Min(levelValues)/diff)*diff, math.Ceil(slices.Max(levelValues)/diff)*diff
+				remainCount := divideCount - int(math.Round((maxLevel-minLevel)/diff))
+				if remainCount >= 0 {
+					if remainCount > 0 {
+						maxLevel += diff * float64(remainCount-remainCount/2)
+						minLevel -= diff * float64(remainCount/2)
+					}
+					yAxisOptions = append(yAxisOptions, YAxisOption{Min: NewFloatPoint(minLevel), Max: NewFloatPoint(maxLevel), DivideCount: divideCount, Unit: 1})
+					break
+				}
+			}
+		}
+		if slices.ContainsFunc(expValues, func(f float64) bool { return f != 0 }) {
 			if levelEnd := strings.Index(levelExp, "("); levelEnd >= 0 {
 				if totalExp := float64(levelExpData.GetInt64("data." + strings.TrimSpace(levelExp[:levelEnd]))); totalExp > 0 {
 					if expPercentStart := strings.Index(levelExp, "("); expPercentStart >= 0 {
 						if expPercentEnd := strings.Index(levelExp, "%"); expPercentEnd >= 0 {
 							if expPercent, err := strconv.ParseFloat(levelExp[expPercentStart+1:expPercentEnd], 64); err == nil {
 								var sumExp, n float64
-								for _, v := range values {
+								for _, v := range expValues {
 									if v != 0 || n > 0 {
 										sumExp += v
 										n++
 									}
 								}
-								aveExp := sumExp / float64(len(values))
+								aveExp := sumExp / float64(len(expValues))
 								days := int(math.Ceil((totalExp - totalExp/100.0*expPercent) / aveExp))
 								s += fmt.Sprintf("预计还有%d天升级\n", days)
 							}
@@ -125,39 +174,36 @@ func findRole(name string) MessageChain {
 					}
 				}
 			}
-			maxValue := slices.Max(values)
-			digits := int(math.Floor(math.Log10(maxValue))) + 1
-			factor := math.Pow(10, float64(digits-1))
-			highest := math.Floor(maxValue / factor)
-			if highest < 2 {
-				factor /= 5
-			} else if highest < 5 {
-				factor /= 2
+			var seriesList []Series
+			seriesList = append(seriesList, NewSeriesFromValues(expValues, ChartTypeBar))
+			if len(levelValues) == len(expValues) {
+				seriesList = append(seriesList, NewSeriesFromValues(levelValues, ChartTypeLine))
+				seriesList[1].AxisIndex = 1
 			}
-			maxValue = math.Ceil(maxValue/factor) * factor
-			p, err := BarRender(
-				[][]float64{values},
+			p, err := Render(
+				ChartOption{SeriesList: seriesList},
 				ThemeOptionFunc(ThemeDark),
-				PaddingOptionFunc(Box{Top: 30, Left: 10, Right: 54, Bottom: 10}),
+				PaddingOptionFunc(Box{Top: 30, Left: 10, Right: 10, Bottom: 10}),
 				XAxisDataOptionFunc(labels),
-				MarkLineOptionFunc(0, SeriesMarkDataTypeAverage),
-				YAxisOptionFunc(YAxisOption{Min: NewFloatPoint(0), Max: &maxValue, DivideCount: int(maxValue / factor)}),
+				//MarkLineOptionFunc(0, SeriesMarkDataTypeAverage),
+				YAxisOptionFunc(yAxisOptions...),
 				func(opt *ChartOption) {
 					opt.XAxis.TextRotation = -math.Pi / 4
 					opt.XAxis.LabelOffset = Box{Top: -5, Left: 7}
 					opt.XAxis.FontSize = 7.5
+					opt.XAxis.FirstAxis = 1
 					opt.ValueFormatter = func(f float64) string {
 						switch {
 						case f < 1000.0:
-							return fmt.Sprintf("%g", f)
+							return fmt.Sprintf("%g", math.Round(f*1000)/1000)
 						case f < 1000000.0:
-							return fmt.Sprintf("%gK", float64(f)/1000.0)
+							return fmt.Sprintf("%gK", math.Round(f)/1000)
 						case f < 1000000000.0:
-							return fmt.Sprintf("%gM", float64(f)/1000000.0)
+							return fmt.Sprintf("%gM", math.Round(f)/1000000)
 						case f < 1000000000000.0:
-							return fmt.Sprintf("%gB", float64(f)/1000000000.0)
+							return fmt.Sprintf("%gB", math.Round(f)/1000000000)
 						default:
-							return fmt.Sprintf("%gT", float64(f)/1000000000000.0)
+							return fmt.Sprintf("%gT", math.Round(f)/1000000000000)
 						}
 					}
 				},
