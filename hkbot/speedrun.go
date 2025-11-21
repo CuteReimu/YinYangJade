7a17b23aea4150b004973116b68c4f08260913c0
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-11-21 15:57:02 +0800

    支持丝之歌查榜
    Co-authored-by: SclicheD <scliched@outlook.com>
    Signed-off-by: 奇葩の灵梦 <cutereimu@vip.qq.com>

---
+++ hkbot/speedrun.go
@@ -0,0 +1,60 @@
+package hkbot
+
+import (
+	"fmt"
+	"log/slog"
+	"slices"
+	"strings"
+
+	"github.com/CuteReimu/YinYangJade/hkbot/scripts"
+	. "github.com/CuteReimu/onebot"
+	"golang.org/x/sync/singleflight"
+)
+
+func init() {
+	addCmdListener(&speedrunLeaderboards{
+		availableInputs: []string{"Any%", "TE", "100%", "Judgement", "Low%", "AB", "Twisted%"},
+		mutedInputs:     []string{"all bosses", "all boss", "allbosses", "allboss"},
+	})
+}
+
+type speedrunLeaderboards struct {
+	availableInputs []string
+	mutedInputs     []string
+	sf              singleflight.Group
+}
+
+func (t *speedrunLeaderboards) Name() string {
+	return "查榜"
+}
+
+func (t *speedrunLeaderboards) ShowTips(int64, int64) string {
+	return "查榜"
+}
+
+func (t *speedrunLeaderboards) CheckAuth(int64, int64) bool {
+	return true
+}
+
+func (t *speedrunLeaderboards) Execute(_ *GroupMessage, content string) MessageChain {
+	content = strings.ToLower(strings.ReplaceAll(content, "%", ""))
+	eq := func(s string) bool {
+		return strings.EqualFold(content, strings.ReplaceAll(s, "%", ""))
+	}
+	if !slices.ContainsFunc(t.availableInputs, eq) && !slices.ContainsFunc(t.mutedInputs, eq) {
+		return MessageChain{&Text{Text: "支持的榜单类型有：" + strings.Join(t.availableInputs, "，")}}
+	}
+	result, err, _ := t.sf.Do(content, func() (any, error) {
+		return scripts.RunPythonScript("speedrun_silksong.py", content)
+	})
+	if err != nil {
+		slog.Error("查询失败", "error", err)
+		return nil
+	}
+	output, ok := result.([]byte)
+	if !ok {
+		slog.Error("查询结果类型错误", "type", fmt.Sprintf("%T", result))
+		return nil
+	}
+	return MessageChain{&Text{Text: string(output)}}
+}
